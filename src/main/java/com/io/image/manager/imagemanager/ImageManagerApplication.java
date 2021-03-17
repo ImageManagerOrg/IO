0590735d507e673bb66e8ed815cdb8ac05f1b233
Przemyslaw5 <przemyslaw.watroba@gmail.com>
2021-03-17 09:51:01 +0100

    Add Spring template

---
+++ src/main/java/com/io/image/manager/imagemanager/ImageManagerApplication.java
@@ -0,0 +1,13 @@
+package com.io.image.manager.imagemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/io/image/manager/imagemanager/ImageManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.image.manager.imagemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
