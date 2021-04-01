0105670b581be40a954a185aa3136f676313876a
Przemyslaw5 <przemyslaw.watroba@gmail.com>
2021-04-01 10:16:28 +0200

    Handle errors in crop operation. Closes #11

--- src/main/java/com/io/image/manager/service/operations/CropOperation.java
+++ src/main/java/com/io/image/manager/service/operations/CropOperation.java
@@ -16,6 +16,7 @@ public class CropOperation extends ImageOperation {
         int startYPoint = Integer.parseInt(getArguments().getOrDefault("y", Integer.toString(0)));
 
         prepareNewDimensionImage(image.getWidth(), image.getHeight(), startXPoint, startYPoint);
+        checkValidationOfArguments(startXPoint, startYPoint, image.getWidth(), image.getHeight());
 
         return image.getSubimage(startXPoint, startYPoint, newWidth, newHeight);
     }
@@ -23,5 +24,29 @@ public class CropOperation extends ImageOperation {
     public void prepareNewDimensionImage(int imageWidth, int imageHeight, int startXPoint, int startYPoint) {
         newWidth = Integer.parseInt(getArguments().getOrDefault("w", Integer.toString(imageWidth - startXPoint)));
         newHeight = Integer.parseInt(getArguments().getOrDefault("h", Integer.toString(imageHeight - startYPoint)));
+    }
+
+    public void checkValidationOfArguments(int startXPoint, int startYPoint, int imageWidth, int imageHeight)
+            throws ImageOperationException {
+
+        // origin point is outside of the original image bounds
+        if (startXPoint < 0 || startYPoint < 0 || startXPoint > imageWidth || startYPoint > imageHeight) {
+            throw new ImageOperationException();
+        }
+
+        // origin point moved by a vector ('w', 'h') is outside of the original image bounds
+        if (startXPoint + newWidth > imageWidth || startYPoint + newHeight > imageHeight){
+            throw new ImageOperationException();
+        }
+
+        // target width or height is negative
+        if (newWidth < 0 || newHeight < 0){
+            throw new ImageOperationException();
+        }
+
+        // target width or height is larger than 2^16
+        if (newWidth > Math.pow(2, 16) || newHeight > Math.pow(2, 16)){
+            throw new ImageOperationException();
+        }
     }
 }
