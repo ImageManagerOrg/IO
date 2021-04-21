4c956918c010325a585af396c5cc08c7ebf0a340
Adam Kania <remilvus@gmail.com>
2021-04-21 16:55:42 +0200

    Create request log

--- src/main/java/com/io/image/manager/controller/ImageController.java
+++ src/main/java/com/io/image/manager/controller/ImageController.java
@@ -18,16 +18,22 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 public class ImageController {
     private final ImageService imageService;
     private final DistributionSummary outboundTrafficSummary;
     private final AppConfigurationProperties props;
+    private final String logPath = System.getProperty("user.home") + "/IM_log.txt";
+    private BufferedWriter writer = null;
 
     public ImageController(ImageService imageService, PrometheusMeterRegistry mr, AppConfigurationProperties props) {
         this.imageService = imageService;
@@ -43,7 +49,6 @@ public class ImageController {
     @RequestMapping(value = "/{filename}", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<Object> getImage(@PathVariable String filename, HttpServletRequest request)
             throws IOException, ImageOperationException {
-
         // TODO: change origin to a one given in request headers, take it from config for now
         var origin = new OriginServer(props.getOriginServer());
 
@@ -53,10 +58,12 @@ public class ImageController {
         image = imageService.fetchAndCacheImage(origin, filename, operations);
 
         if (image.isPresent()) {
+            logRequest(filename, props.getOriginServer(), true, operations);
             byte[] imageArray = dumpImage(image.get());
             outboundTrafficSummary.record(imageArray.length);
             return ResponseEntity.ok(imageArray);
         }
+        logRequest(filename, props.getOriginServer(), false, operations);
 
         throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
@@ -65,5 +72,32 @@ public class ImageController {
         ByteArrayOutputStream bao = new ByteArrayOutputStream();
         ImageIO.write(image, "jpg", bao);
         return bao.toByteArray();
+    }
+
+    private void logRequest(String filename, String origin, boolean isPresent, List<ImageOperation> operations){
+        try {
+            if (writer == null){
+                writer = new BufferedWriter(new FileWriter(logPath, true));
+            }
+            String opNames = operations.stream().map(ImageOperation::getName).collect(Collectors.joining("|"));
+            String opArgs = operations.stream().map(ImageOperation -> ImageOperation
+                    .getArguments().toString())
+                    .collect(Collectors.joining("|"));
+            String timestamp = new Timestamp(System.currentTimeMillis()).toString();
+
+            String[] values = {timestamp, origin, filename, String.valueOf(isPresent), opNames};
+
+            for (String val: values) {
+                writer.append(val);
+                writer.append("`");
+            }
+            writer.append(opArgs);
+            writer.append('\n');
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            writer = null;
+        }
+
     }
 }
