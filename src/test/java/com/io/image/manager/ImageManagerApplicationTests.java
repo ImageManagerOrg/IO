6c5a1e80a723ce5f7fb9d3e9dbd0ca54c776b733
Jakub Perzylo <jakub.perzylo@swmansion.com>
2021-05-10 09:12:17 +0200

    Another one

--- src/test/java/com/io/image/manager/ImageManagerApplicationTests.java
+++ src/test/java/com/io/image/manager/ImageManagerApplicationTests.java
@@ -3,11 +3,8 @@ package com.io.image.manager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ImageManagerApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
