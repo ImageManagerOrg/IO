27433accd8872a9ad0bd52fee79483f0ce20f545
Przemyslaw5 <przemyslaw.watroba@gmail.com>
2021-05-22 23:24:57 +0200

    Fix spelling

--- scripts/loadbalancer/migrate_all_sll_cert.sh
+++ scripts/loadbalancer/migrate_all_sll_cert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir /home/ubuntu/deployment/nginx_certs
+mkdir -p /home/ubuntu/deployment/nginx_certs
 /home/ubuntu/deployment/scripts/convert_ssl.sh /home/ubuntu/secrets/origin_a_com.p12 /home/ubuntu/deployment/nginx_certs/origin_a_com.crt.pem /home/ubuntu/deployment/nginx_certs/origin_a_com.key.pem
 /home/ubuntu/deployment/scripts/convert_ssl.sh /home/ubuntu/secrets/origin_b_com.p12 /home/ubuntu/deployment/nginx_certs/origin_b_com.crt.pem /home/ubuntu/deployment/nginx_certs/origin_b_com.key.pem
