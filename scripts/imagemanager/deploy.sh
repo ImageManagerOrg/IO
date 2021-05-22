78258d6c7e263e000a7ddf0bbe9db8d542532109
Przemyslaw5 <przemyslaw.watroba@gmail.com>
2021-05-22 22:23:21 +0200

    Add scripts

---
+++ scripts/imagemanager/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir -p prometheus
+mv prometheus.yml prometheus/
+
+mv prod-docker-compose.yml docker-compose.yml
+
+sudo docker-compose stop
+sudo docker-compose down
+sudo docker-compose pull
+sudo docker-compose up -d
--- scripts/loadbalancer/deploy.sh
+++ scripts/loadbalancer/deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-mkdir -p prometheus
-mv prometheus.yml prometheus/
-
 mv prod-docker-compose.yml docker-compose.yml
 
 /home/ubuntu/deployment/scripts/migrate_all_sll_cert.sh
